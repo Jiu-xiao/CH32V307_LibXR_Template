442f12ace4de24c23285fc43a59bbf8d6f00e4da
jiu-xiao <2592509183@qq.com>
2025-06-13 18:27:48 +0100

    Init.

---
+++ User/main.c
@@ -0,0 +1,33 @@
+/********************************** (C) COPYRIGHT *******************************
+* File Name          : main.c
+* Author             : WCH
+* Version            : V1.0.0
+* Date               : 2021/06/06
+* Description        : Main program body.
+*********************************************************************************
+* Copyright (c) 2021 Nanjing Qinheng Microelectronics Co., Ltd.
+* Attention: This software (modified or not) and binary are used for 
+* microcontroller manufactured by Nanjing Qinheng Microelectronics.
+*******************************************************************************/
+
+/*
+ *@Note
+ *task1 and task2 alternate printing
+ */
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+/*********************************************************************
+ * @fn      main
+ *
+ * @brief   Main program.
+ *
+ * @return  none
+ */
+int main(void)
+{
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+    SystemInit();
+	SystemCoreClockUpdate();
+}
